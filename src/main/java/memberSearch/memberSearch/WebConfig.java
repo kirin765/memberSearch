a4113707843fd660710d78f8e8ff45d2d7f73a5a
kirin765 <asipalseri@gmail.com>
2022-10-03 02:29:48 +0900

    log filter

--- src/main/java/memberSearch/memberSearch/WebConfig.java
+++ src/main/java/memberSearch/memberSearch/WebConfig.java
@@ -1,10 +1,14 @@
 package memberSearch.memberSearch;
 
 import memberSearch.memberSearch.argumentresolver.LoginMemberArgumentResolver;
+import memberSearch.memberSearch.filter.LogFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import javax.servlet.Filter;
 import java.util.List;
 
 @Configuration
@@ -13,5 +17,15 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(new LoginMemberArgumentResolver());
+    }
+
+    @Bean
+    public FilterRegistrationBean logFilter(){
+        FilterRegistrationBean<Filter> filterFilterRegistrationBean = new FilterRegistrationBean<>();
+        filterFilterRegistrationBean.setFilter(new LogFilter());
+        filterFilterRegistrationBean.setOrder(1);
+        filterFilterRegistrationBean.addUrlPatterns("/*");
+
+        return filterFilterRegistrationBean;
     }
 }
